b35dd62ae8f3a0f644428e520719b6623ba15c91
tommcroj03 <tommcrojo@gmail.com>
2024-09-19 19:49:09 +0200

    collapsible sidebar y fallo top sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -3,26 +3,44 @@
 import React, { use, useState } from "react";
 import Image from "next/image";
 import ActionLogo from "@/../public/action-logo-icon.svg";
-import { Icon, LockIcon, LucideIcon } from "lucide-react";
+import { Home, Icon, LockIcon, LucideIcon, X } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import Link from "next/link";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
   const sidebarClassNames = `fixed flex-col h-[100%] justify-between shadow-xl
       transition-all duration-300 h-full z-40 dark:bg-black-bg overflow-y-auto bg-white
-      w-64 
+      ${isSidebarCollapsed ? "w-0 hidden" : "w-64"} 
       `;
   return (
     <div className={sidebarClassNames}>
-      <div className="flex h-[100%] w-full flex-col justify-start">
-        <div className="dark:bg-black-bg z-50 flex min-h-[58px] w-64 items-center justify-start gap-3 bg-white px-6 pt-3">
+      <div className="flex h-[100%] w-full flex-col">
+        <div className="justify-betweem z-50 flex min-h-[58px] w-64 items-center gap-3 bg-white px-6 pt-3 dark:bg-black-bg">
           <Image src={ActionLogo} alt="Action Logo" width={20} height={20} />
-          <div className="text-xl font-bold text-gray-800 dark:text-white">
-            Action
+          <div className="flex flex-row justify-between">
+            <div className="text-xl font-bold text-gray-800 dark:text-white">
+              Action
+            </div>
+            {isSidebarCollapsed ? null : (
+              <button
+                className="py-3"
+                onClick={() => {
+                  dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+                }}
+              >
+                <X className="align- h-6 w-6 text-gray-700 hover:text-gray-500 dark:text-white" />
+              </button>
+            )}
           </div>
         </div>
         {/* TEAM */}
@@ -36,7 +54,7 @@ const Sidebar = () => {
           />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
-              FullStack dev
+              Primer Proyecto Financiero 🎯
             </h3>
             <div className="mt-1 flex items-start gap-1.5">
               <LockIcon className="mt-[0.07rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
@@ -45,6 +63,9 @@ const Sidebar = () => {
           </div>
         </div>
         {/* NAVBAR LINKS! */}
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+        </nav>
       </div>
     </div>
   );
@@ -54,13 +75,13 @@ interface SidebarLinkProps {
   href: string;
   icon: LucideIcon;
   label: string;
-  isCollapsed: boolean;
+  //  isCollapsed: boolean;
 }
 const SidebarLink = ({
   href,
   icon: Icon,
   label,
-  isCollapsed,
+  //  isCollapsed,
 }: SidebarLinkProps) => {
   const pathname = usePathname();
   const isActive =
@@ -72,6 +93,22 @@ const SidebarLink = ({
     (state) => state.global.isSidebarCollapsed,
   );
 
-  return <Link href={href} className="w-full"></Link>;
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-gray-700${
+          isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""
+        }`}
+      >
+        {isActive && (
+          <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200" />
+        )}
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
 };
 export default Sidebar;
