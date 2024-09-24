7c1e5f90d42a0edc7c10655fb6862a77ab2fc237
tommcroj03 <tommcrojo@gmail.com>
2024-09-24 13:09:41 +0200

    api creation

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,8 +1,78 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"; 
-export const api= createApi({
-    baseQuery: fetchBaseQuery({ baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL}),
-    reducerPath:"api",
-    tagTypes: [],
-    endpoints:(build)=>({}),
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+export interface Project {
+  name: string;
+  description?: string;
+  startDate?: string;
+  endDate?: string;
+}
+export interface User {
+  userId?: number;
+  username: string;
+  email: string;
+  profilePictureUrl?: string;
+  congnitoId?: string;
+  teamId?: number;
+}
+
+export interface Attachment {
+  id: number;
+  fileURL: string;
+  fileName: string;
+  taskId: number;
+  uploadedById: number;
+}
+
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export interface Task {
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startDate?: string;
+  dueDate?: string;
+  points?: number;
+  projectId: number;
+  authorUserId?: number;
+  assignedUserId?: number;
+
+  author: User;
+  assignee: User;
+  comments: Comment[];
+  attachments: Attachment[];
+}
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: ["Projects"],
+  endpoints: (build) => ({
+    getProjects: build.query<Project[], void>({
+      query: () => "projects",
+      providesTags: ["Projects"],
+    }),
+    createProject: build.mutation<Project, Partial<Project>>({
+      query: (project) => ({
+        url: "projects",
+        method: "POST",
+        body: project,
+      }),
+      invalidatesTags: ["Projects"],
+    }),
+  }),
 });
-export const{}=api;
+export const { useGetProjectsQuery, useCreateProjectMutation } = api;
